d803dd93ac77619189b0cb2402c3be24d12a7a04
Dylan Johnston <qdylanj@gmail.com>
2016-02-22 21:31:05 +0100

    I forget what this was called, thx Aaron

--- controllers/controllers.go
+++ controllers/controllers.go
@@ -2,8 +2,4 @@ package controllers
 
 import "github.com/labstack/echo"
 
-func Param(c *echo.Context, key string) string {
-	return c.Param(key)
-}
-
-var GetParam = Param
+var GetParam = (*echo.Context).Param
