be2798808ec27f3a869f96f5cf1498a2728de308
Dylan Johnston <qdylanj@gmail.com>
2016-02-22 21:27:59 +0100

    Get rid of unnecessary funcs

--- main.go
+++ main.go
@@ -1,9 +1,6 @@
 package main
 
 import (
-	"net/http"
-	"strconv"
-
 	"github.com/zqzca/back/controllers"
 	"github.com/zqzca/back/db"
 	"github.com/zqzca/back/models"
@@ -28,34 +25,6 @@ var (
 // Handlers
 //----------
 
-func createUser(c *echo.Context) error {
-	u := &user{
-		ID: seq,
-	}
-	if err := c.Bind(u); err != nil {
-		return err
-	}
-	users[u.ID] = u
-	seq++
-	return c.JSON(http.StatusCreated, u)
-}
-
-func updateUser(c *echo.Context) error {
-	u := new(user)
-	if err := c.Bind(u); err != nil {
-		return err
-	}
-	id, _ := strconv.Atoi(c.Param("id"))
-	users[id].Name = u.Name
-	return c.JSON(http.StatusOK, users[id])
-}
-
-func deleteUser(c *echo.Context) error {
-	id, _ := strconv.Atoi(c.Param("id"))
-	delete(users, id)
-	return c.NoContent(http.StatusNoContent)
-}
-
 func main() {
 	db := db.DatabaseConnect()
 
@@ -70,9 +39,6 @@ func main() {
 	// Route
 	e.Post("/sessions", controllers.SessionCreate)
 	e.Post("/users", controllers.UserCreate)
-	e.Get("/users/:id", controllers.UserGet)
-	e.Patch("/users/:id", updateUser)
-	e.Delete("/users/:id", deleteUser)
 
 	// Start server
 	e.Run(":3001")
