0542caffcb9f17bf2bab0f453dac3e920964487a
Dylan Johnston <qdylanj@gmail.com>
2016-05-05 22:01:09 +0200

    Identifer Func

---
+++ db/migrations/20160505212236_CreateIdentifierFunction.sql
@@ -0,0 +1,23 @@
+-- +goose Up
+CREATE OR REPLACE FUNCTION identifier(len int DEFAULT 10) RETURNS text
+  LANGUAGE plpgsql
+  AS $$
+    DECLARE
+      alphabet text;
+      alphabet_size int;
+    BEGIN
+      alphabet := 'abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789';
+      alphabet_size := character_length(alphabet);
+
+      RETURN array_to_string(
+        array(
+          SELECT substr(alphabet, trunc(random() * alphabet_size)::integer + 1, 1)
+          FROM generate_series(1, len)
+        ),
+        ''
+      );
+    END;
+  $$;
+
+-- +goose Down
+DROP FUNCTION IF EXISTS identifier(int);
