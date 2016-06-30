60f9f01ebaf913fdbe7941f4767f9f6e319f7fa7
Dylan Johnston <qdylanj@gmail.com>
2016-06-30 23:22:27 +0200

    database

---
+++ db/database.go
@@ -0,0 +1,44 @@
+package db
+
+import (
+	"database/sql"
+	"fmt"
+	"sync"
+)
+
+type Executor interface {
+	Exec(query string, args ...interface{}) (sql.Result, error)
+	QueryRow(query string, args ...interface{}) *sql.Row
+	Query(query string, args ...interface{}) (*sql.Rows, error)
+}
+
+var Connection *sql.DB
+var mu sync.Mutex
+
+func StartTransaction() *sql.Tx {
+	tx, err := Connection.Begin()
+
+	if err != nil {
+		fmt.Println("Failed to create transaction", err)
+		return nil
+	}
+
+	return tx
+}
+
+// TxWrapper is used in tests
+func TxWrapper(callback func(*sql.Tx)) {
+	if tx, err := Connection.Begin(); err == nil {
+		defer func() {
+			// Catch panics
+			if r := recover(); r != nil {
+				tx.Rollback()
+			}
+		}()
+
+		callback(tx)
+		tx.Rollback()
+	} else {
+		fmt.Println(err)
+	}
+}
