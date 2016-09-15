e7c02091098c57030adb1b0c3e1b3d2a98536050
Dylan Johnston <qdylanj@gmail.com>
2016-09-15 21:28:38 +0200

    Func for temp file names

--- lib/disk.go
+++ lib/disk.go
@@ -1,12 +1,20 @@
 package lib
 
 import (
+	"fmt"
 	"os"
 	"path/filepath"
+
+	"github.com/satori/go.uuid"
 )
 
 func LocalPath(hash string) string {
 	return filepath.Join("files", hash)
+}
+
+func TempFilePath(prefix string) string {
+	uid := uuid.NewV4()
+	return LocalPath(fmt.Sprintf("%s-%s", prefix, uid.String()))
 }
 
 func ExistsOnDisk(hash string) bool {
